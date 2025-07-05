d722df825677b48c9df416ebb1ee214207d8e6b6
abhay0226 <abhaypandey1800@gmail.com>
2025-07-05 22:26:18 +0530

    my project

---
+++ script.js
@@ -0,0 +1,22 @@
+gsap.from("#btm-left",{
+    x:1400,
+    duration:2.5,
+    delay:1,
+})
+gsap.to("#img4",{
+    rotate:360,
+    delay:1,
+    duration:2
+})
+gsap.to("#img3",{
+    rotate:350,
+    delay:1,
+    duration:2.3
+})
+gsap.to("#img2",{
+  
+    rotate:340,
+    delay:1,
+    duration:2.5
+})
+
